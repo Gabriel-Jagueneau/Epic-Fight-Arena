41631fcc67e24ecb5fb8fee3a2507003157f2f3b
“TheGreatMegalodon” <atomes.600@gmail.com>
2024-10-21 21:59:52 +0200

    cl completed

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ var toggleSelection = function(newID) {
 var checkFetch = function() {
     const currentUrl = window.location.href;
     const page = currentUrl.split('?')[1];
-    console.log(page)
     if (page == undefined) {
         fetchFile('home');
     } else {
@@ -33,7 +32,6 @@ async function fetchFile(id) {
     const file = getFile(id);
     const extension = file.split('.').pop().toLowerCase();
     const fetcher = document.getElementById('fetcher');
-
     if (extension === 'html') {
         const htmlResponse = await fetch(`info/${file}`);
         const htmlData = await htmlResponse.text();
@@ -48,9 +46,79 @@ async function fetchFile(id) {
                 <h1 class="mcfont" style="font-size: 40px;">${jsonData['name']}</h1>
                 <img src="https://raw.githubusercontent.com/TheGreatMegalodon/Epic-Fight-Arena/refs/heads/master/images/banners/home-banner.png" alt="">
             </div>
+
+            <div class="content">
+                ${await createDataFOR(id)}
+            </div>
+
         </section>
         `;
     } else {
         return;
     }
 }
+
+async function createDataFOR(id) {
+    switch(id) {
+        case "cl":
+            const clResponse = await fetch('info/cl.json');
+            const clData = await clResponse.json();
+            return `
+                ${clData.logs.map(i => `
+                    <div class="conte ${i.important?'important':''} ${clData.logs.indexOf(i) == 0?'latest':''}">
+                        <div class="maininfo mcfont">
+                            <div class="date">${i.date}</div>-<div class="date">${i.update}</div>
+                            ${clData.logs.indexOf(i) == 0?'<div class="latest">Latest</div>':''}
+                        </div>
+                        <div class="informations">
+                            <div class="globalTitle">${i.globalTitle}</div>
+                            ${i.content.map(j => `
+                                <div class="secondaryTitle">${j.secondaryTitle}</div>
+                                <div class="changes">${j.changes.map(k => `
+                                    <div class="change"><div class="tpe golden">-</div> ${k}</div>
+                                `).join('')}</div>
+                            `).join('')}
+                        </div>
+                    </div>
+                    `
+                ).join('')}
+                `;
+        
+        case "more":
+            const moreResponse = await fetch('info/more.json');
+            const moreData = await moreResponse.json();
+            return `
+                ${moreData.name}
+                `;
+
+        default:
+            return "";
+    }
+}
+
+var copyText = function(id, text) {
+    const cpy = document.getElementById(id);
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            cpy.style = "background-color: lime; color: black;";
+            cpy.innerHTML = `
+                Copy
+                <div class="dld">done_all</div>
+                `;
+        }).catch(() => {
+            cpy.style = "background-color: red;";
+            cpy.innerHTML = `
+                Error
+                <div class="dld">error</div>
+                `;
+        }).finally(() => {
+            setTimeout(() => {
+                cpy.style = "";
+                cpy.innerHTML = `
+                Copy
+                <div class="dld">sports_esports</div>
+                `;
+            }, 1500);
+        });
+}
+
