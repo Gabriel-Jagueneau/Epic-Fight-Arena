cd00ae2560269b75c5879d112d5e09a0d5fac894
“TheGreatMegalodon” <atomes.600@gmail.com>
2024-10-21 13:34:56 +0200

    test2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var oldselectionID = "home";
+fileList = ['home.html','htp.html','more.json','cl.json'];
+
 var toggleSelection = function(newID, file) {
     const oldItem = document.getElementById(oldselectionID);
     const newItem = document.getElementById(newID);
@@ -7,10 +9,17 @@ var toggleSelection = function(newID, file) {
     newItem.classList.toggle('selected');
 
     oldselectionID = newID;
-    fetchFile(file);
+    fetchFile(file, newID);
+    history.pushState(null, '', `?page=${newID}`);
 }
 
-async function fetchFile(file) {
+var getFile = function(name) {
+    const foundFile = fileList.find(file => file.includes(name));
+    return foundFile
+}
+
+async function fetchFile(id) {
+    const file = getFile(id);
     const extension = file.split('.').pop().toLowerCase();
     const fetcher = document.getElementById('fetcher');
 
@@ -21,7 +30,12 @@ async function fetchFile(file) {
     } else if (extension === 'json') {
         const jsonResponse = await fetch(`info/${file}`);
         const jsonData = await jsonResponse.json();
-        fetcher.innerHTML = jsonData['hello'];
+        id = id.split('.')[0];
+        fetcher.innerHTML = `
+        <section class="${id}">
+            <h1 class="mcfont" style="font-size: 40px;">${jsonData['name']}</h1>
+        </section>
+        `;
     } else {
         return;
     }
